9addce37fca2e373a880d83edc3f7109eb7f019f
UvrajSB <mryuvrajsbhadauria@gmail.com>
2025-04-20 04:19:44 +0530

    structured tools

--- agent/tools/decryption.py
+++ agent/tools/decryption.py
@@ -1,5 +1,18 @@
 import tenseal as ts
 from application_context import get_context
+from tools.serialise_deserealise import serialize_bfvvector, deserialize_bfvvector
+from langchain_core.tools import tool
 
-def list_decryption(el : ts.tensors.bfvvector.BFVVector)->list:
-        return el.decrypt()
+def list_decryption(el : str)->list:
+    """
+    Homomorphic Decryption of a base64-encoded BFV-encrypted vector into a list of plaintext integers.
+
+    Args:
+        el (str): A base64-encoded string representing a TenSEAL BFVVector.
+
+    Returns:
+        list: A list of decrypted integers from the encrypted vector.
+    """
+    print(f"Called decryption with input: {el}")
+    el = deserialize_bfvvector(el)
+    return el.decrypt()
--- agent/tools/list_encryption.py
+++ agent/tools/list_encryption.py
@@ -1,11 +1,21 @@
 import tenseal as ts
 from application_context import get_context
+from tools.serialise_deserealise import serialize_bfvvector, deserialize_bfvvector
 
-def list_encryption(l:list)->list:
+def list_encryption(l:list)-> str:
+    """
+    Homomorphic Encryption of a list of numbers.
+
+    Args:
+        l (list): A list of integers to be encrypted.
+
+    Returns:
+        str: A base64-encoded string representing the encrypted TenSEAL BFVVector.
+    """
     context = get_context()
     if context:
-        print(type(ts.bfv_vector(context, l)))
-        return ts.bfv_vector(context, l)
+        print(f"Called encryption with input: {l}")
+        return serialize_bfvvector(ts.bfv_vector(context, l))
     else:
         return ValueError("No context found while using the tool list_encryption")
      
